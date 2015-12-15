29065b3ade4d4d83a7ce3fc04e96f9d82af09c55
nsrchemie <nsrchemie@gmail.com>
2015-12-15 15:19:12 -0800

    Added POST request functionality for forms and fixed redirect/triplicate model creation error

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import PostForm
 # , PostForm
@@ -11,8 +11,10 @@ def post_list(request):
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
+            post.author = request.user
             post.published_date = timezone.now()
             post.save()
+            return HttpResponseRedirect("/")
     else:
         form = PostForm()
     return render(request, 'blog/post_list.html', {'posts': posts, 'form':form})
